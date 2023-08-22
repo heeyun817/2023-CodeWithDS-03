66f6e85f2d737028b3617539ce574c05d9e73c14
이규민 <90364652+20210815@users.noreply.github.com>
2023-08-23 04:59:05 +0900

    어떤 board에 대한 채팅방인지 model에 수정 필요

--- carpool/channels/views.py
+++ carpool/channels/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse
 from .models import ChatMessage
 import json
+from django.contrib.auth.models import User
 
 def chat_view(request):
     messages = ChatMessage.objects.all()
@@ -9,7 +10,7 @@ def chat_view(request):
 
 def send_message(request):
     if request.method == "POST":
-        sender = "User"  # Change this to the actual sender (e.g., user's username)
+        sender = User.objects.get(pk = request.user.pk)  # Change this to the actual sender (e.g., user's username)
         message = request.POST.get("message")
         ChatMessage.objects.create(sender=sender, message=message)
         return HttpResponse("Message sent successfully.")
@@ -17,6 +18,7 @@ def send_message(request):
 
 def get_messages(request):
     messages = ChatMessage.objects.all()
-    messages_list = [{"sender": message.sender, "message": message.message, "timestamp": message.timestamp.strftime("%Y-%m-%d %H:%M:%S")} for message in messages]
+    print(messages)
+    messages_list = [{"sender": message.sender.username, "message": message.message, "timestamp": message.timestamp.strftime("%Y-%m-%d %H:%M:%S")} for message in messages]
     messages_json = json.dumps(messages_list)
     return HttpResponse(messages_json, content_type="application/json")
