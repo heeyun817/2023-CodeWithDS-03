f46cfaf145551dd921b1f1a073153011d8083518
이규민 <90364652+20210815@users.noreply.github.com>
2023-08-23 21:04:14 +0900

    채팅방 구현완료 유저 다른 거 확인 못함

--- carpool/channels/admin.py
+++ carpool/channels/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from channels.models import ChatMessage
+from channels.models import ChatMessage, ChatRoom
 # Register your models here.
 admin.site.register(ChatMessage)
+admin.site.register(ChatRoom)
--- carpool/channels/views.py
+++ carpool/channels/views.py
@@ -15,16 +15,58 @@ def chat_view(request):
         return redirect("channels:chat", {'messages': messages})
     return render(request, 'chat.html', {'messages': messages})
 
-def send_message(request):
+def send_message(request, pk):
+    chatroom = ChatRoom.objects.get(pk=pk) #해당 채팅방 가져오기
+    messages = ChatMessage.objects.filter(chatRoom=chatroom)
     if request.method == "POST":
         sender = User.objects.get(pk = request.user.pk)  # Change this to the actual sender (e.g., user's username)
         message = request.POST.get("message")
-        ChatMessage.objects.create(sender=sender, message=message)
+        ChatMessage.objects.create(sender=sender, message=message, chatRoom=chatroom)
         return HttpResponse("Message sent successfully.")
     return HttpResponse("Invalid request.")
 
-def get_messages(request):
-    messages = ChatMessage.objects.all()
+def get_messages(request, pk):
+    chatroom = ChatRoom.objects.get(pk=pk) #해당 채팅방 가져오기
+    messages = ChatMessage.objects.filter(chatRoom=chatroom)
     if request.method == "GET" :
       context = {'messages': messages}
-    return render(request, 'messages.html', {'messages': messages})
+    return render(request, 'messages.html', {'messages': messages, 'chatroom':chatroom})
+
+
+# channels/views.py
+
+from django.shortcuts import redirect, get_object_or_404
+from .models import ChatRoom
+from board.models import Board  # 이 부분은 게시물 모델의 위치에 맞게 수정
+
+def create_chat_room(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    
+    # 채팅방 생성 코드 작성
+    #chat_room = ChatRoom()
+    #chat_room.board = board
+    chat_room = ChatRoom.objects.create(board=board)
+    
+    # 유저 그룹에 해당 유저 추가
+    chat_room.user_group.add(request.user)
+    chat_room.user_group.add(board.user)
+    #본인이면 채ㅌ이방 생성하기 버튼 안 보이게 해야 함
+    chat_room.save()
+
+    #chat_room = ChatRoom.objects.create(board=board)  # 예시로 채팅방 생성 코드 작성
+
+    # 생성된 채팅방 페이지로 리다이렉트
+    return redirect('channels:chat_room', pk=chat_room.pk)
+
+
+def chat_view(request, pk):
+    chatroom = ChatRoom.objects.get(pk=pk) #해당 채팅방 가져오기
+    messages = ChatMessage.objects.filter(chatRoom=chatroom)
+    if request.method == "POST":
+        sender = User.objects.get(pk = request.user.pk)  # Change this to the actual sender (e.g., user's username)
+        message = request.POST.get("message")
+        chatroom = chatroom
+        ChatMessage.objects.create(sender=sender, message=message, chatRoom=chatroom)
+        messages = ChatMessage.objects.filter(chatRoom=chatroom)
+        return redirect("channels:chat_room", pk, {'messages': messages , 'chatroom':chatroom})
+    return render(request, 'chat.html', {'messages': messages , 'chatroom':chatroom})
