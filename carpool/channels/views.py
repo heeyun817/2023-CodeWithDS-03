5f8e658975e0e7b6904ee2486bde59c6c099b035
이규민 <90364652+20210815@users.noreply.github.com>
2023-08-23 18:57:12 +0900

    채팅창 구현 중 board에 연결 필요

--- carpool/channels/views.py
+++ carpool/channels/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import ChatMessage
 import json
 from django.contrib.auth.models import User
 
 def chat_view(request):
     messages = ChatMessage.objects.all()
-    context = {'messages': messages}
-    return render(request, 'chat.html', context)
+    if request.method == "GET" :
+      context = {'messages': messages}
+    if request.method == "POST":
+        sender = User.objects.get(pk = request.user.pk)  # Change this to the actual sender (e.g., user's username)
+        message = request.POST.get("message")
+        ChatMessage.objects.create(sender=sender, message=message)
+        messages = ChatMessage.objects.all()
+        return redirect("channels:chat", {'messages': messages})
+    return render(request, 'chat.html', {'messages': messages})
 
 def send_message(request):
     if request.method == "POST":
@@ -18,7 +25,6 @@ def send_message(request):
 
 def get_messages(request):
     messages = ChatMessage.objects.all()
-    print(messages)
-    messages_list = [{"sender": message.sender.username, "message": message.message, "timestamp": message.timestamp.strftime("%Y-%m-%d %H:%M:%S")} for message in messages]
-    messages_json = json.dumps(messages_list)
-    return HttpResponse(messages_json, content_type="application/json")
+    if request.method == "GET" :
+      context = {'messages': messages}
+    return render(request, 'messages.html', {'messages': messages})
