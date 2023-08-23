619345f36947893f04bb6cda1d8022786b169cb6
hmj6589 <hmj6589@gamil.com>
2023-08-23 15:37:01 +0900

    회원가입 페이지 또 수정

--- carpool/accounts/views.py
+++ carpool/accounts/views.py
@@ -1,24 +1,24 @@
-from django.contrib import auth 
-from django.contrib.auth import authenticate
-from django.contrib.auth.forms import UserCreationForm 
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
+from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 import random
-from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
 
 # 회원가입
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            if request.POST['password1'] == request.POST['password2']:
-                # 이메일 주소 검사: @duksung.ac.kr 도메인 확인
-                email = form.cleaned_data['email'] 
-                if not email.endswith('@duksung.ac.kr'):
-                    return render(request, 'signup.html', {'error': '덕성여자대학교 이메일 주소를 사용해야 합니다.'})
-                
-                user = form.save()
+        step = request.POST.get('step', 1)  # 현재 단계를 받아옵니다.
+
+        if step == '1':  # 이메일 작성 단계
+            form = CustomUserCreationForm(request.POST)
+            email = form['email'].value()
             
+            if not email.endswith('@duksung.ac.kr'):
+                error_message = '덕성여자대학교 이메일 주소를 사용해야 합니다.'
+                return render(request, 'signup.html', {'form': form, 'step': '1', 'error': error_message})
+
+            if form.is_valid():
                 # 랜덤 숫자 생성
                 random_number = str(random.randint(1000, 9999))
 
@@ -26,32 +26,55 @@ def signup(request):
                 send_mail(
                     '이메일 인증 코드',
                     f'인증 코드: {random_number}',
-                    'your_email@duksung.ac.kr',  # 보내는 이메일 계정
-                    [user.email],
+                    'hmj6589@gmail.com',  # 보내는 이메일 계정
+                    [email],
                     fail_silently=False,
                 )
-                
-                # 랜덤 숫자를 verify_email 뷰로 전달하도록 수정
-                return render(request, 'verify_email.html', {'random_number': random_number})
+
+                return render(request, 'verify_email.html', {'random_number': random_number, 'email': email, 'step': '2'})
+
+        elif step == '2':  # 이메일 인증 단계
+            user_entered_code = request.POST['verification_code']
+            random_number = request.POST.get('random_number')
+            if user_entered_code == random_number:
+                return render(request, 'signup.html', {'email': request.POST['email'], 'step': '3'})
             else:
-                return render(request, 'signup.html', {'error': '비밀번호가 일치하지 않습니다.'})
-        else:
-            form = UserCreationForm()
-            return render(request, 'signup.html', {'form': form})
+                return render(request, 'verify_email.html', {'error': '인증 코드가 일치하지 않습니다.'})
+
+        elif step == '3':  # 비밀번호 작성 단계
+            email = request.POST['email']
+            return render(request, 'signup.html', {'email': email, 'step': '4'})
+
+        elif step == '4':  # 비밀번호 확인 단계
+            email = request.POST['email']
+            password1 = request.POST['password1']
+            password2 = request.POST['password2']
+            if password1 == password2:
+                # 이메일 주소 검사: @duksung.ac.kr 도메인 확인
+                if not email.endswith('@duksung.ac.kr'):
+                    return render(request, 'signup.html', {'error': '덕성여자대학교 이메일 주소를 사용해야 합니다.'})
+
+                user = CustomUserCreationForm(request.POST).save()  # 회원가입 정보 생성
+                auth_login(request, user)  # 로그인 처리
+                return redirect('home')  # 로그인 이후 페이지로 이동
+            else:
+                return render(request, 'signup.html', {'email': email, 'step': '3', 'error': '비밀번호가 일치하지 않습니다.'})
+
     else:
-        form = UserCreationForm()
-        return render(request, 'signup.html', {'form': form})
+        form = CustomUserCreationForm()
+        return render(request, 'signup.html', {'form': form, 'step': '1'})
+
 
 # 이메일 인증 (랜덤 코드 보내서 인증하는 걸로 했습니다.)
 def verify_email(request):
     if request.method == 'POST':
         user_entered_code = request.POST['verification_code']
-        random_number = request.POST.get('random_number')  # 랜덤 숫자 가져오기
-        if user_entered_code == random_number:  # 사용자가 입력한 코드와 랜덤 코드 비교
-            # 인증 완료 후 로그인 처리
-            user = User.objects.get(email=request.user.email)  # 이메일로 사용자 조회
-            auth.login(request, user)
-            return redirect('/')
+        random_number = request.POST.get('random_number')
+        if user_entered_code == random_number:
+            # 인증 완료 후 회원가입 정보 생성 및 로그인 처리
+            user = form.save()  # 회원가입 정보 생성
+            auth_login(request, user)  # 로그인 처리
+            return redirect('home')  # 로그인 이후 페이지로 이동
         else:
             return render(request, 'verify_email.html', {'error': '인증 코드가 일치하지 않습니다.'})
     else:
@@ -79,5 +102,3 @@ def logout(request):
 # 회원가입, 로그인, 로그아웃 모여있는 
 def home(request):
     return render(request, 'home.html')
-
-# 헐@.. 계좌연결안함 ;;
