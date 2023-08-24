0bbe6289b62b9fb6f0d54ec7143102b6a833e243
heeyun817 <vmffpfm79@naver.com>
2023-08-24 22:06:44 +0900

    프로필 기능 수정, 사용 기록 추가

--- carpool/mypage/models.py
+++ carpool/mypage/models.py
@@ -8,19 +8,9 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     username = models.CharField(max_length=50, unique=True)
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=15, null=True, blank=True)  # 전화번호 필드 추가
+    # phone_number = models.CharField(max_length=15, null=True, blank=True)  # 전화번호 필드 추가
     image = models.ImageField(upload_to='Files/%Y/%m/%d/', null=True, blank=True)
     signup_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.username
-
-
-# # 매칭 기록
-# class Matching(models.Model):
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     board = models.ForeignKey(Board, on_delete=models.CASCADE)
-#     # is_matched = models.BooleanField(default=False)
-#
-#     def __str__(self):
-#         return f'{self.user.username} - {self.board.s_title}'
--- carpool/mypage/views.py
+++ carpool/mypage/views.py
@@ -1,9 +1,13 @@
+from django.contrib.auth import get_user_model
+from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User  # User 모델 가져오기
 from .forms import UserProfileForm
 from .models import UserProfile
 from board.models import Board
+# from channels.models import ChatRoom
+from accounts.models import User
 
 @login_required
 def mypage(request):
@@ -14,7 +18,7 @@ def mypage(request):
     user_profile.username = user.username
     user_profile.email = user.email
     user_profile.signup_date = user.date_joined
-    user_profile.phone_number = user.userprofile.phone_number  # User 모델에서 phone_number 가져오기
+    # user_profile.phone_number = user.userprofile.phone_number  # User 모델에서 phone_number 가져오기
     user_profile.save()
 
     return render(request, 'mypage.html', {'user_profile': user_profile})
@@ -37,20 +41,23 @@ def edit_mypage(request):
     return render(request, 'edit_mypage.html',  {'form': form, 'user_profile': user_profile, 'request_user': request.user})
 
 
-# 사용 기록 (자신이 작성 + 구함 완료까지) -> 추가 버튼 눌러서 하는 것도 구현 해야함
+# 사용 기록 (자신이 작성 + 구함 완료까지) -> 다른 사용자 추가 하는 것도 구현 해야함
 def completed_boards(request):
     user = request.user
-    completed_boards = Board.objects.filter(user=user, completion=True)
+    # 현재 사용자와 연결된 채팅방을 먼저 가져옵니다.
+    # chat_rooms = ChatRoom.objects.filter(user_group=user)
+
+    # 채팅방과 연결된 게시글을 가져옵니다.
+    # completed_boards = Board.objects.filter(Q(completion=True, chatroom__in=chat_rooms) | Q(user=user, completion=True))
+    completed_boards = Board.objects.filter(Q(user=user, completion=True)|Q(member=user)).distinct() #users=user
     return render(request, 'completed_boards.html', {'completed_boards': completed_boards})
 
 
 
 # 다른 유저 프로필 보기
 def view_profile(request, user_id):
-    # 요청한 사용자의 프로필을 가져옵니다.
+    User = get_user_model()
     profile_user = get_object_or_404(User, pk=user_id)
-
-    # UserProfile 모델을 사용하여 사용자의 프로필 정보를 가져옵니다.
     user_profile = get_object_or_404(UserProfile, user=profile_user)
 
     context = {
@@ -58,15 +65,14 @@ def view_profile(request, user_id):
         'user_profile': user_profile,
     }
 
-    return render(request, 'profile_template.html', context)
+    return render(request, 'profile_page.html', context)
 
 
 #<!--게시글 작성자의 프로필로 이동하는 링크--> - board/detail.html에 추가하기
 #<a href="{% url 'mypage:view_profile' board.user.id %}">{{ board.user.username }}</a>
 
 
-
- # 자신이 작성한 글 보기(혹시 몰라서 한 번 넣어본 기능..)
+# 자신이 작성한 글 보기(혹시 몰라서 한 번 넣어본 기능..)
 def my_record(request):
     current_user = request.user
     my_posts = Board.objects.filter(user=current_user, completion=False)
